33ed283c6b35c512a172d1805fa9ffee72450dd3
Puneeth Chaganti <punchagan@gmail.com>
2010-11-05 22:23:11 +0530

    Changed version to 0.3, added a note in README about changes.

--- org2blog.el
+++ org2blog.el
@@ -187,7 +187,7 @@ Set to nil if you don't wish to track posts."
   "Ask before killing buffer")
 (make-variable-buffer-local 'org2blog-buffer-kill-prompt)
 
-(defconst org2blog-version "0.2" 
+(defconst org2blog-version "0.3" 
   "Current version of blog.el")
 
 (defvar org2blog-mode-hook nil
