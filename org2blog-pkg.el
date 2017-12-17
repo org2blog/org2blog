bd2028b6a79daa63fc5481deaed63c4efc681be0
Grant Rettke <gcr@wisdomandwonder.com>
2017-12-16 20:44:05 -0600

    Increment for new metaweblog version.

--- org2blog-pkg.el
+++ org2blog-pkg.el
@@ -1,6 +1,6 @@
-(define-package "org2blog" "1.0.1"
+(define-package "org2blog" "1.0.2"
   "Blog from Org mode to wordpress"
   '((org "8.3")
     (xml-rpc "1.6.12")
-    (metaweblog "1.0.0")
+    (metaweblog "1.0.1")
     (htmlize "1.51")))
