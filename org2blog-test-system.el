b372658863f3d44b512aeec8177ff17d6fcde1e3
Grant Rettke <grant@wisdomandwonder.com>
2019-10-10 22:57:46 -0500

    Default to package code

--- org2blog-test-system.el
+++ org2blog-test-system.el
@@ -2,14 +2,15 @@
 ;;;; Setup
 ;;;;
 
-(add-to-list 'load-path "~/src/htmlize")
-(require 'htmlize)
-(add-to-list 'load-path "~/src/hydra")
-(require 'hydra)
-(add-to-list 'load-path "~/src/xml-rpc")
-(require 'xml-rpc)
-(add-to-list 'load-path "~/src/org2blog")
-(require 'org2blog)
+(when nil ;; Default to running using package instead of source
+  (add-to-list 'load-path "~/src/htmlize")
+  (require 'htmlize)
+  (add-to-list 'load-path "~/src/hydra")
+  (require 'hydra)
+  (add-to-list 'load-path "~/src/xml-rpc")
+  (require 'xml-rpc)
+  (add-to-list 'load-path "~/src/org2blog")
+  (require 'org2blog))
 
 (setq org2blog/wp-blog-alist
       `(("test"
