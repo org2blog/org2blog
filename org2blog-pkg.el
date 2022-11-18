0d435af91c237351f0880536cb3cf21a91041ba4
Grant Rettke <grant@wisdomandwonder.com>
2022-11-17 23:36:25 -0600

    Make v1.1.16 release in master

--- org2blog-pkg.el
+++ org2blog-pkg.el
@@ -1,9 +1,9 @@
-(define-package "org2blog" "1.1.15" "Blog from Org mode to WordPress"
+(define-package "org2blog" "1.1.16" "Blog from Org mode to WordPress"
   '((htmlize "1.56" "https://github.com/hniksic/emacs-htmlize.git")
     (hydra "0.15.0" "https://github.com/abo-abo/hydra.git")
     (xml-rpc "1.6.15" "https://github.com/hexmode/xml-rpc-el.git")
     (writegood-mode "2.2.0" "https://github.com/bnbeckwith/writegood-mode")
-    (metaweblog "1.1.15" "https://github.com/org2blog/org2blog.git"))
+    (metaweblog "1.1.16" "https://github.com/org2blog/org2blog.git"))
   :authors
   '(("Puneeth Chaganti" . "punchagan+org2blog@gmail.com"))
   :maintainer
