f94a4ce6bc456f258a046ae30051a9d20b4e2c45
Grant Rettke <grant@wisdomandwonder.com>
2025-07-21 14:12:46 -0500

    Doc refactor.

--- metaweblog.el
+++ metaweblog.el
@@ -26,6 +26,10 @@
 
 ;;; Commentary:
 
+;; A portion of the code in this file is based on blog.el posted to
+;; http://www.mail-archive.com/gnu-emacs-sources@gnu.org/msg01576.html
+;; copyrighted by Ashish Shukla.
+
 ;; This API operates against WordPress using both the
 ;; XML-RPC MetaWeblog API specification
 ;; https://codex.wordpress.org/XML-RPC_MetaWeblog_API
@@ -38,10 +42,6 @@
 ;; obvious and as expected they are excluded from documentation by
 ;; including them in line at the end of the docstring only to satisfy
 ;; the byte-compiler.
-
-;; A portion of the code in this file is based on blog.el posted to
-;; http://www.mail-archive.com/gnu-emacs-sources@gnu.org/msg01576.html
-;; copyrighted by Ashish Shukla.
 
 ;;; Code:
 
